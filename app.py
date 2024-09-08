12853b8c7bf669923e836e1e62c50eac0dfdfffe
Neelabh02 <neelabhtripathi2002@gmail.com>
2024-09-08 12:25:45 +0530

    first comit

--- app.py
+++ app.py
@@ -17,5 +17,25 @@ standard_scaler=pickle.load(open('models/scaler.pkl','rb'))
 def index():
     return render_template('index.html')
 
-if __name__=="__main":
+@app.route('/predictdata',methods=['GET','POST'])
+def predict_datapoint():
+    if request.method=="POST":
+        Temperature=float(reuest.form.get('Temperature'))
+        RH=float(reuest.form.get('RH'))
+        Ws=float(reuest.form.get('Ws'))
+        Rain=float(reuest.form.get('Rain'))
+        FFMC=float(reuest.form.get('FFMC'))
+        DMC=float(reuest.form.get('DMC'))
+        ISI=float(reuest.form.get('ISI'))
+        Classes=float(reuest.form.get('Classes'))
+        Region=float(reuest.form.get('Region'))
+
+        new_data_scaled=standard_scaler.transform([[Temperature,RH,Ws,Rain,FFMC,DMC,ISI,Classes,Region]])
+        result=ridge_model.predict(new_data_scaled)
+
+        return render_template('home.html',results=result[0 ])
+    else:
+        return render_template('home.html')    
+
+if __name__=="__main__":
     app.run(host="0.0.0.0")
